1a56b089fa12e9d6c529f1ed5918b9ed16219792
Сергей Ефимов <efimovsergeiv@gmail.com>
2023-12-26 10:39:47 +0300

    rm:

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,14 @@
-/** @type {import('tailwindcss').Config} */
+/** @type { import('tailwindcss').Config} */
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
     extend: {},
       fontFamily: {
-        'sans': ['Caveat', ],
-        'serif': ['Caveat', ],
-        'mono': ['Caveat', ],
-        'display': ['Caveat', ],
-        'body': ['Caveat', ],
+        'sans': ['Open Sans', ],
+        'serif': ['Open Sans', ],
+        'mono': ['Open Sans', ],
+        'display': ['Open Sans', ],
+        'body': ['Open Sans', ],
       },
   },
   plugins: [],
